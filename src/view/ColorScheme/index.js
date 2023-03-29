0c1c54b2f618ba077b264ba534364b5b95f27193
BiswasSuva <biswassuvadeep6@gmail.com>
2023-03-30 00:49:48 +0530

    up

--- src/view/ColorScheme/index.js
+++ src/view/ColorScheme/index.js
@@ -8,7 +8,7 @@ import { useNavigate } from "react-router-dom";
 import Swal from "sweetalert2";
 
 function Index() {
-  
+
   const screen = useScreen();
   const navigate = useNavigate();
   const [logo, setLogo] = useState("");
@@ -47,18 +47,18 @@ function Index() {
     }
   };
   return (
-    
-        <>
-        <div className="row">
-          <div className="col-lg-12 col-xl-12 col-md-12 col-12">
-            <div className="heading-title">
-              <p>This colors will be used as primary in your app.</p>
-            </div>
+
+    <>
+      <div className="row">
+        <div className="col-lg-12 col-xl-12 col-md-12 col-12">
+          <div className="heading-title">
+            <p>This colors will be used as primary in your app.</p>
           </div>
         </div>
-        <div className="row mt-4" style={{ justifyContent: "center" }}>
-          <div className="col-lg-4 col-xl-4 col-md-4 col-12">
-            {/* <div className="colorbutton">
+      </div>
+      <div className="row mt-4 justify-content-center" >
+        {/* <div className="col-lg-4 col-xl-4 col-md-4 col-12">
+            <div className="colorbutton">
               <div className="center">
                 <div className="pin" />
               </div>
@@ -78,53 +78,62 @@ function Index() {
                   <li className="color" />
                 </ul>
               </div>
-            </div> */}
-          </div>
-          <div className="col-lg-4 col-xl-4 col-md-4 col-12 d-flex align-items-center">
-            {!logo ? (
-              <p style={{ color: "#fff" }}>Please upload logo</p>
-            ) : (
-              <div className="logoUploadarea">
-                <img src={logo} className="img-fluid" alt="Uploading Logo" />
-              </div>
-            )}
-          </div>
-          <div className="col-lg-4 col-xl-4 col-md-4 col-12">
-            <div className="chooseBtn">
-              <button className="btn " onClick={getColor}>
-                Choose Colour from Logo
-              </button>
             </div>
+          </div> */}
+        <div className="col-lg-4 col-xl-4 col-md-4 col-12 d-flex align-items-center ">
+          {!logo ? (
+            <p style={{ color: "#fff" }}>Please upload logo</p>
+          ) : (
+            <div className="logoUploadarea">
+              <img src={logo} className="img-fluid" alt="Uploading Logo" />
+            </div>
+          )}
+        </div>
+        <div className="col-lg-4 col-xl-4 col-md-4 col-12">
+          <div className="chooseBtn">
+            <button className="btn " onClick={getColor}>
+              Choose Colour from Logo
+            </button>
           </div>
         </div>
-        <div className="row mt-5">
-          <div className="col-lg-12 col-md-12 col-xl-12">
-             {/* <SketchPicker /> */}
-            <div className="colour-picker">
-             
-              <label>Primary Colour</label>
-              <input
-                value={primaryColor}
-                onChange={(val) => setPrimaryColor(val.target.value)}
-                type="color"
-                className="form-control mb-3"
-              />
+      </div>
+      <div className="row mt-5">
+        <div className="col-lg-12 col-md-12 col-xl-12">
+          {/* <SketchPicker /> */}
+          <div className="colour-picker">
 
-              <label>Secondary Colour</label>
-              <input
-                value={secondaryColor}
-                onChange={(val) => setSecondaryColor(val.target.value)}
-                type="color"
-                className="form-control"
-              />
-              <button className="btn " onClick={Submit}>
-                Confirm Color
-              </button>
-            </div>
+            <label>Primary Colour</label>
+            <input
+              value={primaryColor}
+              onChange={(val) => setPrimaryColor(val.target.value)}
+              type="color"
+              className="form-control"
+            />
+
+            <label>Secondary Colour</label>
+            <input
+              value={secondaryColor}
+              onChange={(val) => setSecondaryColor(val.target.value)}
+              type="color"
+              className="form-control"
+            />
 
           </div>
+
         </div>
-        </>
+      </div>
+
+      <div className="row mt-5">
+        <div className="col-12">
+          <div className="colour-picker">
+            <button className="btn " onClick={Submit}>
+              Confirm Color
+            </button>
+          </div>
+
+        </div>
+      </div>
+    </>
 
   );
 }
