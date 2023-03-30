0850f18c7a6e3ed47da5dcc031da9f87bab67011
BiswasSuva <biswassuvadeep6@gmail.com>
2023-03-31 03:06:55 +0530

    up

--- src/view/Dashboard/Tutorial.js
+++ src/view/Dashboard/Tutorial.js
@@ -1,9 +1,50 @@
 import React from "react";
-import phone from "../../images/phone.png";
+// import phone from "../../images/phone.png";
+import phone from "../../images/iPhone13.png";
+
 import FirstImage from "../../images/tutorial/firstImage.png";
 import { motion, AnimatePresence } from "framer-motion";
-
+import Slider from "react-slick";
 function Tutorial({ onClose }) {
+
+  var settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    arrows: true,
+    autoplay: false,
+    slidesToScroll: 1,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: false,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 1164,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: false,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  };
+
   return (
     <>
       <div className="modal_body" />
@@ -25,24 +66,65 @@ function Tutorial({ onClose }) {
         </div>
         <div className="row">
           <div className="col-xl-8 col-lg-8 col-md-12 col-12">
-            <div className="modal_content_box">
-              <p>1. Click Where it says Page Title on the app Screen</p>
-              <p>
-                2. From the properties Panel on the right side of the screen,
-                change the text from Page Title to Home
-              </p>
-            </div>
-            <div className="modal_btn">
-              <span className="prev_btn">
-                <i className="fa-solid fa-angle-left" />
-              </span>
-              <span className="next_btn">
-                <i className="fa-solid fa-angle-right" />
-              </span>
+            <div className="tutorialslider">
+              <Slider {...settings}>
+                <div>
+                  <div className="modal_content_box">
+                    <p>1. Click Where it says Page Title on the app Screen</p>
+                    <p>
+                      2. From the properties Panel on the right side of the screen,
+                      change the text from Page Title to Home
+                    </p>
+                  </div>
+                  {/* <div className="modal_btn">
+                    <span className="prev_btn">
+                      <i className="fa-solid fa-angle-left" />
+                    </span>
+                    <span className="next_btn">
+                      <i className="fa-solid fa-angle-right" />
+                    </span>
+                  </div> */}
+                </div>
+                <div>
+                  <div className="modal_content_box">
+                    <p>1. Click Where it says Page Title on the app Screen</p>
+                    <p>
+                      2. From the properties Panel on the right side of the screen,
+                      change the text from Page Title to Home
+                    </p>
+                  </div>
+                  {/* <div className="modal_btn">
+                    <span className="prev_btn">
+                      <i className="fa-solid fa-angle-left" />
+                    </span>
+                    <span className="next_btn">
+                      <i className="fa-solid fa-angle-right" />
+                    </span>
+                  </div> */}
+                </div>
+                <div>
+                  <div className="modal_content_box">
+                    <p>1. Click Where it says Page Title on the app Screen</p>
+                    <p>
+                      2. From the properties Panel on the right side of the screen,
+                      change the text from Page Title to Home
+                    </p>
+                  </div>
+                  {/* <div className="modal_btn">
+                    <span className="prev_btn">
+                      <i className="fa-solid fa-angle-left" />
+                    </span>
+                    <span className="next_btn">
+                      <i className="fa-solid fa-angle-right" />
+                    </span>
+                  </div> */}
+                </div>
+
+              </Slider>
             </div>
           </div>
           <div className="col-xl-4 col-lg-4 col-md-12 col-12">
-            <div className="image_bg mt-3">
+            <div className="image_bg">
               <div className="modal_image">
                 <img className="img-fluid" src={phone} alt="phone" />
               </div>
