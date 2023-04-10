fa0aed7f8fde77a851dcc932ccf7cb3292037dce
suvadeepB <suvadeepbiswas456@gmail.com>
2023-04-10 15:26:54 +0530

    up

--- src/view/Dashboard/Mainmobile.js
+++ src/view/Dashboard/Mainmobile.js
@@ -3,7 +3,7 @@ import changethis from "../../images/changethis.png"
 function Mainmobile({children}) {
     return (
         <div className='mainMobile'>
-            <div className="iphone">
+            <div className="iphone" >
                 {/* <div className="screen" /> */}
                     {children}
                 
