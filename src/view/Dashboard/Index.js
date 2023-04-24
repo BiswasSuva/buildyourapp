8ec5e09bccafbea9456a65ed7caaa64611b4c2e7
BiswasSuva <biswassuvadeep6@gmail.com>
2023-04-24 13:29:53 +0530

    dashboard

--- src/view/Dashboard/Index.js
+++ src/view/Dashboard/Index.js
@@ -59,6 +59,33 @@ function Index() {
   }
 
 
+
+  const series2 = [
+    70
+  ]
+
+  const options2 = {
+    chart: {
+      height: 350,
+      type: 'radialBar',
+    },
+    plotOptions: {
+      radialBar: {
+        hollow: {
+          size: '70%',
+        }
+      },
+    },
+    // labels: ['Cricket'],
+    legend: {
+      position: 'right',
+      offsetY: 0,
+      height: 230,
+    }
+  }
+
+
+
   const name = [
     {
       name: 'Social Media',
@@ -129,15 +156,15 @@ function Index() {
 
       <main id="sectionEnrollment" style={{ marginRight: "0", marginTop: "40px" }}>
         <div className="container-fluid">
-          <div className="row">
+          {/* <div className="row">
             <div className="col-6">
               <div className="cardlist">
                 <div className="details">
                   <h2>Welcome, <span className="name">John</span></h2>
-                  <p>Number of Reports</p>
+                  <p></p>
                 </div>
                 <div className="numbercard">
-                  <h3>102</h3>
+                  <h3></h3>
                 </div>
               </div>
             </div>
@@ -152,11 +179,10 @@ function Index() {
                 </div>
               </div>
             </div>
-          </div>
-
+          </div> */}
 
           <div className="row mt-4">
-            <div className="col-3">
+            <div className="col-3 mb-4">
               <div className="smallcardlist">
                 <div className="smalldetails">
                   <h6 style={{ color: "#3f51b5" }}>66,163k</h6>
@@ -171,7 +197,7 @@ function Index() {
               </div>
             </div>
 
-            <div className="col-3">
+            <div className="col-3 mb-4">
               <div className="smallcardlist">
                 <div className="smalldetails">
                   <h6 style={{ color: "#e64a19" }}>3,409k</h6>
@@ -186,7 +212,7 @@ function Index() {
               </div>
             </div>
 
-            <div className="col-3">
+            <div className="col-3 mb-4">
               <div className="smallcardlist">
                 <div className="smalldetails">
                   <h6 style={{ color: "#4caf50" }}>5,355k</h6>
@@ -201,7 +227,7 @@ function Index() {
               </div>
             </div>
 
-            <div className="col-3">
+            <div className="col-3 mb-4">
               <div className="smallcardlist">
                 <div className="smalldetails">
                   <h6 style={{ color: "#ffa000" }}>66,163k</h6>
@@ -209,10 +235,47 @@ function Index() {
                 </div>
                 <div className="numbercard">
                   <h3><i class="fa-solid fa-list-check" style={{ color: "#ffa000" }}></i></h3>
+
                 </div>
               </div>
               <div>
                 <img src={orange} alt="" className="imagebg" style={{ maxWidth: "100%", height: "auto" }} />
+              </div>
+            </div>
+
+            <div className="col-3 mb-4">
+              <div className="smallcardlist">
+                <div className="smalldetails">
+                  <h6 style={{ color: "#ffa000" }}>Welcome, <span className="name">John</span></h6>
+                  <p>Number of Reports</p>
+                </div>
+                <div className="numbercard">
+                  {/* <h3><i class="fa-solid fa-list-check" style={{ color: "#ffa000" }}></i></h3> */}
+                  <h3>102</h3>
+                </div>
+              </div>
+              <div>
+                {/* <img src={orange} alt="" className="imagebg" style={{ maxWidth: "100%", height: "auto" }} /> */}
+                <img src={green} alt="" className="imagebg" style={{ maxWidth: "100%", height: "auto" }} />
+              </div>
+            </div>
+
+            <div className="col-3 mb-4">
+              <div className="smallcardlist">
+                <div className="smalldetails">
+                  <h6 style={{ color: "#ffa000" }}>Active</h6>
+                  <p>Leads / Contacts</p>
+                </div>
+                <div className="numbercard">
+                  {/* <h3><i class="fa-solid fa-list-check" style={{ color: "#ffa000" }}></i></h3> */}
+                  <h3>325</h3>
+                </div>
+              </div>
+              <div>
+                <img src={orange} alt="" className="imagebg" style={{ maxWidth: "100%", height: "auto" }} />
+                <div className="numbercard">
+
+                </div>
               </div>
             </div>
           </div>
@@ -238,9 +301,45 @@ function Index() {
                 <Chart options={options1} series={series1} type="donut" width="100%" />
               </div>
 
+              <div className="chartbg" style={{ marginBottom: 0 }}>
+                <div className="d-flex align-items-center justify-content-between">
+                  <h5 className="charttitle">Earning Reports</h5>
+                  <div className="float-right dropdown show">
+                    <button
+                      type="button"
+                      className=" dropdown-toggle"
+                      data-toggle="dropdown"
+                    >
+                      <i class="fa-solid fa-ellipsis-vertical"></i>
+                    </button>
+                    <div className="dropdown-menu">
+                      <a className="dropdown-item" href="#">
+                        Sales Report
+                      </a>
+                      <a className="dropdown-item" href="#">
+                        Export Report
+                      </a>
+                      <a className="dropdown-item" href="#">
+                        Profit
+                      </a>
+                      <a className="dropdown-item" href="#">
+                        Action
+                      </a>
+                    </div>
+                  </div>
+                </div>
+
+                <Chart options={options2} series={series2} type="radialBar" width="100%" />
+                <div className="text-center">
+                  <p className="text-muted text-truncate mb-2">Weekly Earnings</p>
+                  <h5>$2,523</h5>
+                </div>
+
+              </div>
+
             </div>
-            <div className="col-6">
-              <div className="chartbg">
+            <div className="col-8"  >
+              <div className="chartbg" style={{ height: "100%" }}>
                 <div className="d-flex align-items-center justify-content-between">
                   <h5 className="charttitle">Revenues YTD</h5>
                   <div role="group" className="mb-2 btn-group">
@@ -263,91 +362,131 @@ function Index() {
 
           <div className="row">
             <div className="col-12">
-              <div className="table-responsive">
-                <table className="table">
-                  <thead>
-                    <tr className="bg-warning">
-                      <th>Order</th>
-                      <th>Item Name</th>
-                      <th>Quantity</th>
-                      <th>Product Code</th>
-                      <th>Vendor</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr className="bg-danger">
-                      <td>3091</td>
-                      <td>Arch</td>
-                      <td>1</td>
-                      <td>1105</td>
-                      <td>Imptus</td>
+              <div className="d-flex justify-content-between align-items-center" style={{background: "#fff", borderRadius: "10px 10px 0 0 ", padding: "2%"}}>
+                <h4 className="card-title" style={{marginBottom: 0}}>Latest Transactions</h4>
+                <div className="float-right dropdown show">
+                  <button
+                    type="button"
+                    className=" dropdown-toggle"
+                    data-toggle="dropdown"
+                  >
+                    <i class="fa-solid fa-ellipsis-vertical"></i>
+                  </button>
+                  <div className="dropdown-menu">
+                    <a className="dropdown-item" href="#">
+                      Sales Report
+                    </a>
+                    <a className="dropdown-item" href="#">
+                      Export Report
+                    </a>
+                    <a className="dropdown-item" href="#">
+                      Profit
+                    </a>
+                    <a className="dropdown-item" href="#">
+                      Action
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div className="chartbg" style={{borderRadius: "0 0 10px 10px", paddingTop: "0"}}>
+                <div className="table-responsive">
+                  <table className="table table-bordered custom-dashboardtable">
+                    <thead>
+                      {/* <tr className="bg-warning"> */}
+                      <tr className="">
+                        <th>Order</th>
+                        <th>Item Name</th>
+                        <th>Quantity</th>
+                        <th>Product Code</th>
+                        <th>Vendor</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {/* <tr className="bg-danger"> */}
+                      <tr >
+                        <td>3091</td>
+                        <td>Arch</td>
+                        <td>1</td>
+                        <td>1105</td>
+                        <td>Imptus</td>
 
-                    </tr>
-                    <tr className="table-primary">
-                      <td>3091</td>
-                      <td>Arch</td>
-                      <td>1</td>
-                      <td>1105</td>
-                      <td>Imptus</td>
-                    </tr>
-                    <tr className="table-success">
-                      <td>3091</td>
-                      <td>Arch</td>
-                      <td>1</td>
-                      <td>1105</td>
-                      <td>Imptus</td>
-                    </tr>
-                    <tr className="table-danger">
-                      <td>3091</td>
-                      <td>Arch</td>
-                      <td>1</td>
-                      <td>1105</td>
-                      <td>Imptus</td>
-                    </tr>
-                    <tr className="table-info">
-                      <td>3091</td>
-                      <td>Arch</td>
-                      <td>1</td>
-                      <td>1105</td>
-                      <td>Imptus</td>
-                    </tr>
-                    <tr className="table-warning">
-                      <td>3091</td>
-                      <td>Arch</td>
-                      <td>1</td>
-                      <td>1105</td>
-                      <td>Imptus</td>
-                    </tr>
-                    <tr className="bg-success">
-                      <td>3091</td>
-                      <td>Arch</td>
-                      <td>1</td>
-                      <td>1105</td>
-                      <td>Imptus</td>
-                    </tr>
-                    <tr className="table-secondary">
-                      <td>3091</td>
-                      <td>Arch</td>
-                      <td>1</td>
-                      <td>1105</td>
-                      <td>Imptus</td>
-                    </tr>
-                    <tr className="table-light">
-                      <td>3091</td>
-                      <td>Arch</td>
-                      <td>1</td>
-                      <td>1105</td>
-                      <td>Imptus</td>
-                    </tr>
-                    <tr className="table-dark text-dark">
-                      <td>3091</td>
-                      <td>Arch</td>
-                      <td>1</td>
-                      <td>1105</td>
-                      <td>Imptus</td>
-                    </tr>
-                  </tbody>
-                </table>
+                      </tr>
+                      {/* <tr className="table-primary"> */}
+                      <tr >
+                        <td>3091</td>
+                        <td>Arch</td>
+                        <td>1</td>
+                        <td>1105</td>
+                        <td>Imptus</td>
+                      </tr>
+                      {/* <tr className="table-success"> */}
+                      <tr>
+                        <td>3091</td>
+                        <td>Arch</td>
+                        <td>1</td>
+                        <td>1105</td>
+                        <td>Imptus</td>
+                      </tr>
+                      {/* <tr className="table-danger"> */}
+                      <tr>
+                        <td>3091</td>
+                        <td>Arch</td>
+                        <td>1</td>
+                        <td>1105</td>
+                        <td>Imptus</td>
+                      </tr>
+                      {/* <tr className="table-info"> */}
+                      <tr >
+                        <td>3091</td>
+                        <td>Arch</td>
+                        <td>1</td>
+                        <td>1105</td>
+                        <td>Imptus</td>
+                      </tr>
+                      {/* <tr className="table-warning"> */}
+                      <tr>
+                        <td>3091</td>
+                        <td>Arch</td>
+                        <td>1</td>
+                        <td>1105</td>
+                        <td>Imptus</td>
+                      </tr>
+
+                      {/* <tr className="bg-success"> */}
+                      <tr>
+                        <td>3091</td>
+                        <td>Arch</td>
+                        <td>1</td>
+                        <td>1105</td>
+                        <td>Imptus</td>
+                      </tr>
+                      {/* <tr className="table-secondary"> */}
+                      <tr >
+                        <td>3091</td>
+                        <td>Arch</td>
+                        <td>1</td>
+                        <td>1105</td>
+                        <td>Imptus</td>
+                      </tr>
+                      {/* <tr className="table-light"> */}
+                      <tr >
+                        <td>3091</td>
+                        <td>Arch</td>
+                        <td>1</td>
+                        <td>1105</td>
+                        <td>Imptus</td>
+                      </tr>
+                      {/* <tr className="table-dark text-dark"> */}
+                      <tr>
+                        <td>3091</td>
+                        <td>Arch</td>
+                        <td>1</td>
+                        <td>1105</td>
+                        <td>Imptus</td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
               </div>
             </div>
           </div>
